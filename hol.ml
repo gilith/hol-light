22ab671fc23954d762277ac8af6a98510a01b895
Joe Hurd <joe@gilith.com>
2011-01-31 14:34:42 -0800

    Verified parsers

--- hol.ml
+++ hol.ml
@@ -168,6 +168,12 @@ loads "define.ml";;     (* Support for general recursive definitions         *)
 loads "help.ml";;       (* Online help using the entries in Help directory   *)
 loads "database.ml";;   (* List of name-theorem pairs for search system      *)
 
+(* ------------------------------------------------------------------------- *)
+(* Additions to the standard library.                                        *)
+(* ------------------------------------------------------------------------- *)
+
+loads "opentheory/examples/stdlib.ml";;
+
 (* Uncomment to extract the standard theory library
 stop_logging ();;
 *)
--- opentheory/examples/parser.ml
+++ opentheory/examples/parser.ml
@@ -2,34 +2,6 @@
 (* A type of parse streams.                                                  *)
 (* ------------------------------------------------------------------------- *)
 
-logfile "option-case-def";;
-
-let option_distinct = distinctness "option";;
-
-export_thm option_distinct;;
-
-let option_inj = injectivity "option";;
-
-export_thm option_inj;;
-
-let option_cases = prove_cases_thm option_INDUCT;;
-
-export_thm option_cases;;
-
-let case_option_def = new_recursive_definition option_RECURSION
-  `(case_option f b NONE = (b:B)) /\
-   (case_option f b (SOME a) = f (a:A))`;;
-
-export_thm case_option_def;;
-
-logfile "list-case-def";;
-
-let case_list_def = new_recursive_definition list_RECURSION
-  `(case_list f b [] = (b:B)) /\
-   (case_list f b (CONS h t) = f (h:A) t)`;;
-
-export_thm case_list_def;;
-
 logfile "parser-stream-def";;
 
 let stream_induct,stream_recursion = define_type
