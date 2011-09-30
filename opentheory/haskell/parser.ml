615c92e9685141bd942b57520de56168756368ea
Joe Hurd <joe@gilith.com>
2011-09-30 14:14:31 -0700

    Progress towards Haskell

--- opentheory/haskell/parser.ml
+++ opentheory/haskell/parser.ml
@@ -6,8 +6,7 @@ logfile "haskell-parser-src";;
 
 (* Streams *)
 
-export_haskell_thm
-  (CONJ stream_induct (CONJ stream_recursion case_stream_def));;
+export_haskell_thm case_stream_def;;
 
 export_haskell_thm length_stream_def;;
 
@@ -19,9 +18,37 @@ export_haskell_thm list_to_stream_def;;
 
 (* Parsers *)
 
-export_haskell_thm parser_tybij;;
+export_haskell_thm (CONJUNCT1 parser_tybij);;
 
 export_haskell_thm parse_def;;
+
+export_haskell_thm parser_none_def;;
+
+export_haskell_thm parse_none_def;;
+
+export_haskell_thm parser_all_def;;
+
+export_haskell_thm parse_all_def;;
+
+export_haskell_thm parser_partial_map_def;;
+
+export_haskell_thm parse_partial_map_def;;
+
+export_haskell_thm parse_map_def;;
+
+export_haskell_thm parser_pair_def;;
+
+export_haskell_thm parse_pair_def;;
+
+export_haskell_thm parse_option_def;;
+
+export_haskell_thm parse_some_def;;
+
+export_haskell_thm (REWRITE_RULE [FORALL_AND_THM] parse_stream_def);;
+
+(* ------------------------------------------------------------------------- *)
+(* Testing.                                                                  *)
+(* ------------------------------------------------------------------------- *)
 
 logfile "haskell-parser-test";;
 
