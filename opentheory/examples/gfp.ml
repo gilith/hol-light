6714d28120fdf6a783944300690e9f855145aea3
Joe Hurd <joe@gilith.com>
2011-11-18 10:10:19 -0800

    GF(p) division

--- opentheory/examples/gfp.ml
+++ opentheory/examples/gfp.ml
@@ -144,29 +144,29 @@ let gfp_one_nonzero = new_axiom
    `~(num_to_gfp 1 = num_to_gfp 0)`;;
 *)
 
-logfile "gfp-inverse-def";;
+logfile "gfp-div-def";;
 
 (*PARAMETRIC
-(* gfp-inverse-def *)
+(* gfp-div-def *)
 *)
 
-let gfp_inverse_exists = prove
-  (`!n : gfp. ~(n = num_to_gfp 0) ==> ?m. gfp_mult m n = num_to_gfp 1`,
+let gfp_inv_exists = prove
+  (`!x : gfp. ~(x = num_to_gfp 0) ==> ?y. gfp_mult y x = num_to_gfp 1`,
    REPEAT STRIP_TAC THEN
-   MP_TAC (SPECL [`oddprime`; `gfp_to_num n`] gcd_prime) THEN
+   MP_TAC (SPECL [`oddprime`; `gfp_to_num x`] gcd_prime) THEN
    ANTS_TAC THENL
    [REWRITE_TAC [oddprime_prime] THEN
-    MP_TAC (SPECL [`oddprime`; `gfp_to_num n`] divides_mod) THEN
+    MP_TAC (SPECL [`oddprime`; `gfp_to_num x`] divides_mod) THEN
     REWRITE_TAC [oddprime_nonzero; gfp_to_num_mod_bound] THEN
     DISCH_THEN SUBST1_TAC THEN
     STRIP_TAC THEN
-    UNDISCH_TAC `~(n = num_to_gfp 0)` THEN
+    UNDISCH_TAC `~(x = num_to_gfp 0)` THEN
     REWRITE_TAC [] THEN
     MATCH_MP_TAC gfp_to_num_inj THEN
     ASM_REWRITE_TAC [num_to_gfp_to_num; zero_mod_oddprime];
     ALL_TAC] THEN
    STRIP_TAC THEN
-   MP_TAC (SPECL [`oddprime`; `gfp_to_num n`] egcd_exists) THEN
+   MP_TAC (SPECL [`oddprime`; `gfp_to_num x`] egcd_exists) THEN
    POP_ASSUM SUBST1_TAC THEN
    REWRITE_TAC [DIST_CASES] THEN
    STRIP_TAC THENL
@@ -187,33 +187,68 @@ let gfp_inverse_exists = prove
       [num_to_gfp_mult; num_to_gfp_oddprime; gfp_mult_right_zero;
        gfp_neg_zero]]);;
 
-let gfp_mult_left_inverse =
-    let th0 = ONCE_REWRITE_RULE [RIGHT_IMP_EXISTS_THM] gfp_inverse_exists in
+let gfp_mult_left_inv =
+    let th0 = ONCE_REWRITE_RULE [RIGHT_IMP_EXISTS_THM] gfp_inv_exists in
     let th1 = REWRITE_RULE [SKOLEM_THM] th0 in
-    new_specification ["gfp_inverse"] th1;;
+    new_specification ["gfp_inv"] th1;;
 
-export_thm gfp_mult_left_inverse;;
+export_thm gfp_mult_left_inv;;
 
 (*PARAMETRIC
-new_constant ("gfp_inverse", `:gfp -> gfp`);;
+new_constant ("gfp_inv", `:gfp -> gfp`);;
 
-let gfp_mult_left_inverse = new_axiom
-   `!n. ~(n = num_to_gfp 0) ==> gfp_mult (gfp_inverse n) n = num_to_gfp 1`;;
+let gfp_mult_left_inv = new_axiom
+   `!x. ~(x = num_to_gfp 0) ==> gfp_mult (gfp_inv x) x = num_to_gfp 1`;;
 *)
 
-logfile "gfp-inverse-thm";;
+let gfp_mult_left_div =
+    let def = new_definition `!x y. gfp_div x y = gfp_mult x (gfp_inv y)` in
+    prove
+    (`!x y. ~(x = num_to_gfp 0) ==> gfp_div (gfp_mult x y) x = y`,
+     REPEAT STRIP_TAC THEN
+     REWRITE_TAC [def] THEN
+     CONV_TAC (LAND_CONV (REWR_CONV gfp_mult_comm)) THEN
+     REWRITE_TAC [GSYM gfp_mult_assoc] THEN
+     MP_TAC (SPEC `x : gfp` gfp_mult_left_inv) THEN
+     ANTS_TAC THENL
+     [FIRST_ASSUM ACCEPT_TAC;
+      DISCH_THEN SUBST1_TAC THEN
+      MATCH_ACCEPT_TAC gfp_mult_left_one]);;
 
-let gfp_mult_right_inverse = prove
-  (`!n. ~(n = num_to_gfp 0) ==> gfp_mult n (gfp_inverse n) = num_to_gfp 1`,
-   GEN_TAC THEN
-   ONCE_REWRITE_TAC [gfp_mult_comm] THEN
-   MATCH_ACCEPT_TAC gfp_mult_left_inverse);;
-
-export_thm gfp_mult_right_inverse;;
+export_thm gfp_mult_left_div;;
 
 (*PARAMETRIC
-let gfp_mult_right_inverse = prove
-   `!n. ~(n = num_to_gfp 0) ==> gfp_mult n (gfp_inverse n) = num_to_gfp 1`;;
+new_constant ("gfp_div", `:gfp -> gfp -> gfp`);;
+
+let gfp_mult_left_div = new_axiom
+   `!x y. ~(x = num_to_gfp 0) ==> gfp_div (gfp_mult x y) x = y`;;
+*)
+
+logfile "gfp-div-thm";;
+
+let gfp_mult_right_inv = prove
+  (`!x. ~(x = num_to_gfp 0) ==> gfp_mult x (gfp_inv x) = num_to_gfp 1`,
+   GEN_TAC THEN
+   ONCE_REWRITE_TAC [gfp_mult_comm] THEN
+   MATCH_ACCEPT_TAC gfp_mult_left_inv);;
+
+export_thm gfp_mult_right_inv;;
+
+(*PARAMETRIC
+let gfp_mult_right_inv = prove
+   `!x. ~(x = num_to_gfp 0) ==> gfp_mult x (gfp_inv x) = num_to_gfp 1`;;
+*)
+
+let gfp_mult_right_div = prove
+  (`!x y. ~(x = num_to_gfp 0) ==> gfp_div (gfp_mult y x) x = y`,
+   ONCE_REWRITE_TAC [gfp_mult_comm] THEN
+   ACCEPT_TAC gfp_mult_left_div);;
+
+export_thm gfp_mult_right_div;;
+
+(*PARAMETRIC
+let gfp_mult_right_div = new_axiom
+   `!x y. ~(x = num_to_gfp 0) ==> gfp_div (gfp_mult y x) x = y`;;
 *)
 
 let gfp_mult_left_cancel = prove
@@ -224,7 +259,7 @@ let gfp_mult_left_cancel = prove
     ASM_CASES_TAC `x = num_to_gfp 0` THEN
     ASM_REWRITE_TAC [] THEN
     ONCE_REWRITE_TAC [GSYM gfp_mult_left_one] THEN
-    MP_TAC (SPEC `x : gfp` gfp_mult_left_inverse) THEN
+    MP_TAC (SPEC `x : gfp` gfp_mult_left_inv) THEN
     ASM_REWRITE_TAC [] THEN
     DISCH_THEN (fun th -> ONCE_REWRITE_TAC [GSYM th]) THEN
     ASM_REWRITE_TAC [gfp_mult_assoc];
@@ -277,89 +312,89 @@ let gfp_mult_right_cancel_one = new_axiom
    `!x y. gfp_mult y x = x <=> x = num_to_gfp 0 \/ y = num_to_gfp 1`;;
 *)
 
-let gfp_inverse_nonzero = prove
-  (`!x. ~(x = num_to_gfp 0) ==> ~(gfp_inverse x = num_to_gfp 0)`,
+let gfp_inv_nonzero = prove
+  (`!x. ~(x = num_to_gfp 0) ==> ~(gfp_inv x = num_to_gfp 0)`,
    REPEAT STRIP_TAC THEN
-   MP_TAC (SPEC `x : gfp` gfp_mult_left_inverse) THEN
+   MP_TAC (SPEC `x : gfp` gfp_mult_left_inv) THEN
    ASM_REWRITE_TAC [gfp_mult_left_zero; gfp_one_nonzero]);;
 
-export_thm gfp_inverse_nonzero;;
+export_thm gfp_inv_nonzero;;
 
 (*PARAMETRIC
-let gfp_inverse_nonzero = new_axiom
-   `!x. ~(x = num_to_gfp 0) ==> ~(gfp_inverse x = num_to_gfp 0)`;;
+let gfp_inv_nonzero = new_axiom
+   `!x. ~(x = num_to_gfp 0) ==> ~(gfp_inv x = num_to_gfp 0)`;;
 *)
 
-let gfp_inverse_inverse = prove
-  (`!x. ~(x = num_to_gfp 0) ==> gfp_inverse (gfp_inverse x) = x`,
+let gfp_inv_inv = prove
+  (`!x. ~(x = num_to_gfp 0) ==> gfp_inv (gfp_inv x) = x`,
    REPEAT STRIP_TAC THEN
-   MP_TAC (SPEC `x : gfp` gfp_inverse_nonzero) THEN
+   MP_TAC (SPEC `x : gfp` gfp_inv_nonzero) THEN
    ASM_REWRITE_TAC [] THEN
    STRIP_TAC THEN
-   MP_TAC (SPEC `gfp_inverse x` gfp_mult_left_cancel) THEN
+   MP_TAC (SPEC `gfp_inv x` gfp_mult_left_cancel) THEN
    ASM_REWRITE_TAC [] THEN
    DISCH_THEN (fun th -> ONCE_REWRITE_TAC [GSYM th]) THEN
-   ASM_SIMP_TAC [gfp_mult_right_inverse; gfp_mult_left_inverse]);;
+   ASM_SIMP_TAC [gfp_mult_right_inv; gfp_mult_left_inv]);;
 
-export_thm gfp_inverse_inverse;;
+export_thm gfp_inv_inv;;
 
 (*PARAMETRIC
-let gfp_inverse_inverse = new_axiom
-   `!x. ~(x = num_to_gfp 0) ==> gfp_inverse (gfp_inverse x) = x`;;
+let gfp_inv_inv = new_axiom
+   `!x. ~(x = num_to_gfp 0) ==> gfp_inv (gfp_inv x) = x`;;
 *)
 
-let gfp_inverse_inj = prove
+let gfp_inv_inj = prove
   (`!x y.
       ~(x = num_to_gfp 0) /\ ~(y = num_to_gfp 0) /\
-      gfp_inverse x = gfp_inverse y ==>
+      gfp_inv x = gfp_inv y ==>
       x = y`,
    REPEAT STRIP_TAC THEN
-   MP_TAC (SPEC `x : gfp` gfp_inverse_inverse) THEN
+   MP_TAC (SPEC `x : gfp` gfp_inv_inv) THEN
    ASM_REWRITE_TAC [] THEN
    DISCH_THEN SUBST_VAR_TAC THEN
-   MATCH_MP_TAC gfp_inverse_inverse THEN
+   MATCH_MP_TAC gfp_inv_inv THEN
    ASM_REWRITE_TAC []);;
 
-export_thm gfp_inverse_inj;;
+export_thm gfp_inv_inj;;
 
 (*PARAMETRIC
-let gfp_inverse_inj = new_axiom
+let gfp_inv_inj = new_axiom
    `!x y.
       ~(x = num_to_gfp 0) /\ ~(y = num_to_gfp 0) /\
-      gfp_inverse x = gfp_inverse y ==>
+      gfp_inv x = gfp_inv y ==>
       x = y`;;
 *)
 
-let gfp_inverse_one = prove
-  (`gfp_inverse (num_to_gfp 1) = num_to_gfp 1`,
+let gfp_inv_one = prove
+  (`gfp_inv (num_to_gfp 1) = num_to_gfp 1`,
    MP_TAC (SPEC `num_to_gfp 1` gfp_mult_left_cancel) THEN
    REWRITE_TAC [gfp_one_nonzero] THEN
    DISCH_THEN (fun th -> ONCE_REWRITE_TAC [GSYM th]) THEN
    REWRITE_TAC [gfp_mult_right_one] THEN
-   MATCH_MP_TAC gfp_mult_right_inverse THEN
+   MATCH_MP_TAC gfp_mult_right_inv THEN
    ACCEPT_TAC gfp_one_nonzero);;
 
-export_thm gfp_inverse_one;;
+export_thm gfp_inv_one;;
 
 (*PARAMETRIC
-let gfp_inverse_one = new_axiom
-   `gfp_inverse (num_to_gfp 1) = num_to_gfp 1`;;
+let gfp_inv_one = new_axiom
+   `gfp_inv (num_to_gfp 1) = num_to_gfp 1`;;
 *)
 
-let gfp_inverse_is_one = prove
+let gfp_inv_is_one = prove
   (`!x.
-      ~(x = num_to_gfp 0) /\ gfp_inverse x = num_to_gfp 1 ==>
+      ~(x = num_to_gfp 0) /\ gfp_inv x = num_to_gfp 1 ==>
       x = num_to_gfp 1`,
    REPEAT STRIP_TAC THEN
-   MATCH_MP_TAC gfp_inverse_inj THEN
-   ASM_REWRITE_TAC [gfp_inverse_one; gfp_one_nonzero]);;
+   MATCH_MP_TAC gfp_inv_inj THEN
+   ASM_REWRITE_TAC [gfp_inv_one; gfp_one_nonzero]);;
 
-export_thm gfp_inverse_is_one;;
+export_thm gfp_inv_is_one;;
 
 (*PARAMETRIC
-let gfp_inverse_is_one = new_axiom
+let gfp_inv_is_one = new_axiom
    `!x.
-      ~(x = num_to_gfp 0) /\ gfp_inverse x = num_to_gfp 1 ==>
+      ~(x = num_to_gfp 0) /\ gfp_inv x = num_to_gfp 1 ==>
       x = num_to_gfp 1`;;
 *)
 
@@ -386,11 +421,11 @@ let gfp_mult_eq_zero = new_axiom
       gfp_mult x y = num_to_gfp 0 <=> x = num_to_gfp 0 \/ y = num_to_gfp 0`;;
 *)
 
-let gfp_inverse_mult = prove
+let gfp_inv_mult = prove
   (`!x y.
       ~(x = num_to_gfp 0) /\ ~(y = num_to_gfp 0) ==>
-      gfp_mult (gfp_inverse x) (gfp_inverse y) =
-      gfp_inverse (gfp_mult x y)`,
+      gfp_mult (gfp_inv x) (gfp_inv y) =
+      gfp_inv (gfp_mult x y)`,
    REPEAT STRIP_TAC THEN
    CONV_TAC (LAND_CONV (REWR_CONV gfp_mult_comm)) THEN
    MP_TAC (SPEC `gfp_mult x y` gfp_mult_left_cancel) THEN
@@ -401,24 +436,24 @@ let gfp_inverse_mult = prove
    STRIP_TAC THENL
    [ONCE_REWRITE_TAC [gfp_mult_assoc] THEN
     CONV_TAC (LAND_CONV (RAND_CONV (REWR_CONV (GSYM gfp_mult_assoc)))) THEN
-    MP_TAC (SPEC `y : gfp` gfp_mult_right_inverse) THEN
+    MP_TAC (SPEC `y : gfp` gfp_mult_right_inv) THEN
     ASM_REWRITE_TAC [] THEN
     DISCH_THEN SUBST1_TAC THEN
     REWRITE_TAC [gfp_mult_left_one] THEN
-    MATCH_MP_TAC gfp_mult_right_inverse THEN
+    MATCH_MP_TAC gfp_mult_right_inv THEN
     FIRST_ASSUM ACCEPT_TAC;
     MATCH_MP_TAC EQ_SYM THEN
-    MATCH_MP_TAC gfp_mult_right_inverse THEN
+    MATCH_MP_TAC gfp_mult_right_inv THEN
     ASM_REWRITE_TAC [gfp_mult_eq_zero]]);;
 
-export_thm gfp_inverse_mult;;
+export_thm gfp_inv_mult;;
 
 (*PARAMETRIC
-let gfp_inverse_mult = new_axiom
+let gfp_inv_mult = new_axiom
    `!x y.
       ~(x = num_to_gfp 0) /\ ~(y = num_to_gfp 0) ==>
-      gfp_mult (gfp_inverse x) (gfp_inverse y) =
-      gfp_inverse (gfp_mult x y)`;;
+      gfp_mult (gfp_inv x) (gfp_inv y) =
+      gfp_inv (gfp_mult x y)`;;
 *)
 
 logfile_end ();;
