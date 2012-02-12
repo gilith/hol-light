8ed601f51db5807bc2d934abd9db750070954463
Joe Hurd <joe@gilith.com>
2012-02-11 23:05:38 -0800

    Tweak

--- opentheory/cloud.ml
+++ opentheory/cloud.ml
@@ -1,8 +1,8 @@
 (* ------------------------------------------------------------------------- *)
-(* A general web tactic.                                                     *)
+(* A general cloud tactic.                                                   *)
 (* ------------------------------------------------------------------------- *)
 
-let WEB_TAC url =
+let CLOUD_TAC url =
   let tac (asl,g) =
       let goal_file = Filename.temp_file "goal" ".art" in
       let proof_file = Filename.temp_file "proof" ".art" in
@@ -14,14 +14,14 @@ let WEB_TAC url =
       let curl = Sys.command cmd in
       let () =
           if curl = 0 then ()
-          else failwith ("WEB_TAC: curl exit code nonzero: " ^
+          else failwith ("CLOUD_TAC: curl exit code nonzero: " ^
                          string_of_int curl) in
       let th =
           match import_article proof_file with
-            [] -> failwith "WEB_TAC: no theorems in resulting article"
+            [] -> failwith "CLOUD_TAC: no theorems in resulting article"
           | [th] -> th
           | _ :: _ :: _ ->
-            failwith "WEB_TAC: multiple theorems in resulting article" in
+            failwith "CLOUD_TAC: multiple theorems in resulting article" in
       ACCEPT_TAC th in
   W tac;;
 
@@ -29,8 +29,8 @@ let WEB_TAC url =
 (* Known instances of cloud tactics.                                         *)
 (* ------------------------------------------------------------------------- *)
 
-let QBF_TAC = WEB_TAC "http://cam.xrchz.net/holqbf.cgi"
-and SKICO_TAC = WEB_TAC "http://cam.xrchz.net/skico.cgi";;
+let QBF_TAC = CLOUD_TAC "http://cam.xrchz.net/holqbf.cgi"
+and SKICO_TAC = CLOUD_TAC "http://cam.xrchz.net/skico.cgi";;
 
 (* ------------------------------------------------------------------------- *)
 (* Tests.                                                                    *)
