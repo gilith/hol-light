8532e529f4fcb6f8b1a18d9975eaf9109dfe8dc7
Joe Hurd <joe@gilith.com>
2012-02-13 01:27:40 -0800

    Examples for paper

--- opentheory/cloud.ml
+++ opentheory/cloud.ml
@@ -3,6 +3,8 @@
 (* Joe Hurd                                                                  *)
 (* ========================================================================= *)
 
+#load "unix.cma";;
+
 (* ------------------------------------------------------------------------- *)
 (* A general interface for calling cloud tactics.                            *)
 (* ------------------------------------------------------------------------- *)
@@ -128,17 +130,37 @@ let CLOUD_CONV url ths =
     let goal_file = Filename.temp_file "goal" ".art" in
     let proof_file = Filename.temp_file "proof" ".art" in
     let goal = mk_icomb (mk_icomb (`K : A -> B -> A`,`F`), tm) in
+    let t0 = Unix.gettimeofday () in
     let () = export_goal goal_file ([],goal) in
+    let t1 = Unix.gettimeofday () in
+    let goal_time = t1 -. t0 in
+    let goal_size = (Unix.stat goal_file).Unix.st_size in
     let cmd =
         "curl --silent --show-error " ^ url ^
         " --form \"article=@" ^ goal_file ^ "\"" ^
         " --output " ^ proof_file in
     try let curl = Sys.command cmd in
+        let t2 = Unix.gettimeofday () in
+        let curl_time = t2 -. t1 in
         let () =
             if curl = 0 then ()
             else failwith ("CLOUD_CONV: curl exit code nonzero: " ^
                            string_of_int curl) in
         let (asms,thms) = Import.read_article context proof_file in
+        let t3 = Unix.gettimeofday () in
+        let proof_time = t3 -. t2 in
+        let proof_size = (Unix.stat proof_file).Unix.st_size in
+        let () =
+            let size_to_string n = string_of_int n in
+            let time_to_string t = string_of_int (truncate (1000.0 *. t)) in
+            let total_time = goal_time +. curl_time +. proof_time in
+            report (" & " ^
+                    size_to_string goal_size ^ " & " ^
+                    time_to_string goal_time ^ " & " ^
+                    time_to_string curl_time ^ " & " ^
+                    size_to_string proof_size ^ " & " ^
+                    time_to_string proof_time ^ " & " ^
+                    time_to_string total_time ^ " \\\\") in
         let th =
             match thms with
               [] -> failwith "CLOUD_CONV: no theorems in resulting article"
@@ -180,8 +202,27 @@ let QBF_CONV =
      TAUT `!t. ((T <=> t) <=> t) /\ ((t <=> T) <=> t) /\
                ((F <=> t) <=> ~t) /\ ((t <=> F) <=> ~t)`;
      TAUT `(p <=> q \/ r) <=> (p \/ ~q) /\ (p \/ ~r) /\ (q \/ r \/ ~p)`;
-     TAUT `(p <=> q /\ r) <=> (p \/ ~q \/ ~r) /\ (q \/ ~p) /\ (r \/ ~p)`];;
+     TAUT `(p <=> q /\ r) <=> (p \/ ~q \/ ~r) /\ (q \/ ~p) /\ (r \/ ~p)`;
+     TAUT `!x. x \/ ~x`;
+     TAUT `!t1 t2 t3. t1 /\ t2 /\ t3 <=> (t1 /\ t2) /\ t3`;
+     TAUT `!P Q. (?(x:A). P /\ Q x) <=> P /\ (?x. Q x)`;
+     prove (`!P Q. (!(x:A). P x /\ Q x) <=> (!x. P x) /\ (!x. Q x)`,
+            ACCEPT_TAC FORALL_AND_THM);
+     prove (`F <=> (!x. x)`, ACCEPT_TAC F_DEF);
+     TAUT `!t1 t2. (t1 ==> t2) ==> (t2 ==> t1) ==> (t1 <=> t2)`;
+     TAUT `~(t /\ ~t)`;
+     TAUT `!t. F ==> t`;
+     TAUT `!t. (T ==> t <=> t) /\ (t ==> T <=> T) /\ (F ==> t <=> T) /\
+               (t ==> t <=> T) /\ (t ==> F <=> ~t)`;
+     prove (`(\/) = (\t1 t2. !t. (t1 ==> t) ==> (t2 ==> t) ==> t)`,
+            ACCEPT_TAC OR_DEF)];;
 
+(* QBF_CONV `!x y z. (x \/ y) /\ (x \/ z)`;; Broken *)
+QBF_CONV `x \/ ~x`;;
+QBF_CONV `x /\ ~x`;;
+(* QBF_CONV `(x /\ x) \/ !y. x ==> y`;; Broken *)
+QBF_CONV `!x. x \/ ~x`;;
+QBF_CONV `?p. (!q. (p \/ ~q)) /\ !q:bool. ?r. r`;;
 QBF_CONV `?x. !y. ?z. (~x \/ ~y) /\ (~z \/ ~y)`;;
 
 let SKICO_CONV =
@@ -190,6 +231,9 @@ let SKICO_CONV =
      TAUT `!t1 t2. (t1 ==> t2) ==> (t2 ==> t1) ==> (t1 <=> t2)`;
      prove (`!(x:A). x = x <=> T`, REWRITE_TAC []);
      prove (`!(f : C -> B) (g : A -> C). f o g = (\x. f (g x))`,
-            MATCH_ACCEPT_TAC o_DEF)];;
+            MATCH_ACCEPT_TAC o_DEF);
+     prove (`(K : A -> B -> A) = (\x v. x)`, ACCEPT_TAC K_DEF)];;
 
+SKICO_CONV `!x. x \/ ~x`;;
+SKICO_CONV `?p. (!q. (p \/ ~q)) /\ !q:bool. ?r. r`;;
 SKICO_CONV `?x. !y. ?z. (~x \/ ~y) /\ (~z \/ ~y)`;;
