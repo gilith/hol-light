a1659039823513955cd77eddf6609f84f8032682
Joe Leslie-Hurd <joe@gilith.com>
2013-03-12 05:06:06 +0000

    More details

--- opentheory/theories/hardware/hardware.ml
+++ opentheory/theories/hardware/hardware.ml
@@ -8,7 +8,7 @@
 (* ------------------------------------------------------------------------- *)
 
 extend_the_interpretation
-  "opentheory/theories/montgomery/montgomery.int";;
+  "opentheory/theories/hardware/hardware.int";;
 
 (* ------------------------------------------------------------------------- *)
 (* Why higher order logic is a good formalism for specifying and verifying   *)
@@ -17,7 +17,15 @@ extend_the_interpretation
 (* 1. http://www.cl.cam.ac.uk/~mjcg/WhyHOL.pdf                               *)
 (* ------------------------------------------------------------------------- *)
 
+(* ------------------------------------------------------------------------- *)
+(* Definition of wire devices.                                               *)
+(* ------------------------------------------------------------------------- *)
+
 logfile "hardware-def";;
+
+(* ------------------------------------------------------------------------- *)
+(* Wires.                                                                    *)
+(* ------------------------------------------------------------------------- *)
 
 let (mk_dest_wire,dest_mk_wire) =
   CONJ_PAIR (define_newtype ("w","wire") ("s",`:bool stream`));;
@@ -32,7 +40,6 @@ let signal_def = new_definition
 
 export_thm signal_def;;
 
-(***
 let power_def =
   let def = new_definition `power = mk_wire (sconstant T)` in
   prove
@@ -48,25 +55,24 @@ let ground_def =
      REWRITE_TAC [signal_def; def; wire_tybij; snth_sconstant]);;
 
 export_thm ground_def;;
-***)
 
 (* ------------------------------------------------------------------------- *)
-(* Basic devices.                                                            *)
+(* Primitive wire devices.                                                   *)
 (* ------------------------------------------------------------------------- *)
 
-let delay_def = new_definition
+let flipflop_def = new_definition
   `!x y.
      flipflop x y <=>
      !t. signal x t = signal y (t + 1)`;;
 
-export_thm delay_def;;
+export_thm flipflop_def;;
 
 let not_def = new_definition
   `!x y.
      not x y <=>
      !t. signal y t = ~signal x t`;;
 
-export_thm notgate_def;;
+export_thm not_def;;
 
 let and2_def = new_definition
   `!x1 x2 y.
@@ -124,22 +130,101 @@ let adder3_def = new_definition
 export_thm adder3_def;;
 
 (* ------------------------------------------------------------------------- *)
-(* Bus devices.                                                              *)
+(* Properties of wire devices.                                               *)
+(* ------------------------------------------------------------------------- *)
+
+logfile "hardware-thm";;
+
+(***
+let xor3 = prove
+ (`!x1 x2 x3 y.
+     xor3 x1 x2 x3 y <=>
+     !t. signal y t = (signal x1 t = (signal x2 t = signal x3 t))`,
+  REPEAT GEN_TAC THEN
+  REWRITE_TAC [xor3_def; xor2_def] THEN
+  EQ_TAC THENL
+  [REPEAT STRIP_TAC THEN
+   ASM_REWRITE_TAC [] THEN
+   BOOL_CASES_TAC `signal x1 t` THEN
+   BOOL_CASES_TAC `signal x2 t` THEN
+   REWRITE_TAC [];
+   STRIP_TAC THEN
+   EXISTS_TAC
+
+export_thm xor3;;
+***)
+
+(***
+let adder3 = prove
+ (`!x y z s c t.
+     adder3 x y z s c ==>
+     bits_to_num [signal x t] + bits_to_num [signal y t] +
+     bits_to_num [signal z t] =
+     bits_to_num [signal s t] + 2 * bits_to_num [signal c t]`,
+  REPEAT GEN_TAC THEN
+  REWRITE_TAC [adder3_def; bits_to_num_def; MULT_0; ZERO_ADD] THEN
+  STRIP_TAC THEN
+
+export_thm adder3;;
+***)
+
+(* ------------------------------------------------------------------------- *)
+(* Buses.                                                                    *)
+(* ------------------------------------------------------------------------- *)
+
+let (mk_dest_bus,dest_mk_bus) =
+  CONJ_PAIR (define_newtype ("b","bus") ("l",`:wire list`));;
+
+export_thm mk_dest_bus;;
+export_thm dest_mk_bus;;
+
+let bus_tybij = CONJ mk_dest_bus dest_mk_bus;;
+
+let width_def = new_definition
+  `!b. width b = LENGTH (dest_bus b)`;;
+
+export_thm width_def;;
+
+let wire_def = new_definition
+  `!b i. wire b i = nth (dest_bus b) i`;;
+
+export_thm wire_def;;
+
+let bsignal_def = new_definition
+  `!b t. bsignal b t = MAP (\w. signal w t) (dest_bus b)`;;
+
+export_thm bsignal_def;;
+
+(* ------------------------------------------------------------------------- *)
+(* Definition of bus devices.                                                *)
 (* ------------------------------------------------------------------------- *)
 
 let compressor32_def = new_definition
   `!x y z s c.
      compressor32 x y z s c <=>
      ?n.
-       LENGTH x = n /\
-       LENGTH y = n /\
-       LENGTH z = n /\
-       LENGTH s = n /\
-       LENGTH c = n /\
+       width x = n /\ width y = n /\ width z = n /\
+       width s = n /\ width c = n /\
        !i.
          i < n ==>
-         adder3 (nth x i) (nth y i) (nth z i) (nth s i) (nth c i)`;;
+         adder3 (wire x i) (wire y i) (wire z i) (wire s i) (wire c i)`;;
 
+export_thm compressor32_def;;
 
+(* ------------------------------------------------------------------------- *)
+(* Properties of bus devices.                                                *)
+(* ------------------------------------------------------------------------- *)
+
+(***
+let compressor32 = prove
+ (`!x y z s c t.
+     compressor32 x y z s c ==>
+     bits_to_num (bsignal x t) + bits_to_num (bsignal y t) +
+     bits_to_num (bsignal z t) =
+     bits_to_num (bsignal s t) + 2 * bits_to_num (bsignal c t)`,
+  REPEAT GEN_TAC THEN
+  REWRITE_TAC [compressor32_def] THEN
+  STRIP_TAC THEN
+***)
 
 logfile_end ();;
